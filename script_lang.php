f1b87a37b1f04a9e4b06e6add199dd21c5cae228
giaco944 <130480481+giaco944@users.noreply.github.com>
2025-02-26 21:01:37 +0100

    Update script_lang.php

--- script_lang.php
+++ script_lang.php
@@ -1,11 +1,4 @@
 <?php
-// Définit le type de contenu en texte brut pour éviter l'exécution HTML
-header("Content-Type: text/plain");
-
-// Exécute la commande 'ls' ou 'dir' selon l'OS
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-    system("dir");
-} else {
-    system("ls -la");
-}
+// Affiche toutes les informations de configuration PHP
+phpinfo();
 ?>
